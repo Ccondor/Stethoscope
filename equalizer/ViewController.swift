5a138b0d40be1c553ff20436a71ca36e73f5ee25
Sergio Sebastian Arriagada <sergio@hashdog.com>
2017-04-25 12:47:38 -0300

    Initial Commit

---
+++ equalizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  equalizer
+//
+//  Created by Sergio Sebastián Arriagada on 25/4/17.
+//  Copyright © 2017 Hashdog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
